3f4b2442ea49cfef77a4d57593e09e93e0b0cd24
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-26 12:04:43 -0300

    Imports e Create EndPoint do GetFuncionarios

--- FrontEndAngular/src/app/services/funcionario.service.ts
+++ FrontEndAngular/src/app/services/funcionario.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Funcionario } from '../models/Funcionarios';
+import { Response } from '../models/Response';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class FuncionarioService {
 
     private apiUrl = `${environment.ApiUrl}/Funcionario`
 
+    constructor(private http: HttpClient) { }
 
-  constructor() { }
+    GetFuncionarios(): Observable<Response<Funcionario[]>> {
+        return this.http.get<Response<Funcionario[]>>(this.apiUrl);
+    }
 }
