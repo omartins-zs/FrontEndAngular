7979707ba62c17d146aff8039161cb34843cbb5b
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-26 11:50:14 -0300

    Adicionando e Testando ngOnInit e Fazendo Imports

--- FrontEndAngular/src/app/pages/home/home.component.ts
+++ FrontEndAngular/src/app/pages/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Funcionario } from 'src/app/models/Funcionarios';
+import { FuncionarioService } from 'src/app/services/funcionario.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    funcionarios: Funcionario[] = [];
+    funcionariosGeral: Funcionario[] = [];
 
+    constructor(private funcionarioService: FuncionarioService) { }
+
+
+    ngOnInit(): void {
+        this.funcionarioService.GetFuncionarios().subscribe(
+            data => {
+                console.log(data);
+            },
+            error => {
+                console.error('Ocorreu um erro ao obter os funcionários:', error);
+            }
+        );
+    }
 }
