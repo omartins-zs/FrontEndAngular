6db1c62372e525d6882132f382ea8c369a13be11
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-26 13:42:04 -0300

    Criando Funcionario-Form Component

---
+++ FrontEndAngular/src/app/components/funcionario-form/funcionario-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-funcionario-form',
+    templateUrl: './funcionario-form.component.html',
+    styleUrls: ['./funcionario-form.component.css']
+})
+
+export class FuncionarioFormComponent  {
+
+}
+
