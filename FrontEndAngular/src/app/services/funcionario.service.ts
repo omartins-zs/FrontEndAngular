bad46375b235af1c7817638e29f0bc824e6927c7
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-26 12:00:39 -0300

    Criando a variavel apiUrl e Importando Environment

--- FrontEndAngular/src/app/services/funcionario.service.ts
+++ FrontEndAngular/src/app/services/funcionario.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FuncionarioService {
+
+    private apiUrl = `${environment.ApiUrl}/Funcionario`
+
 
   constructor() { }
 }
