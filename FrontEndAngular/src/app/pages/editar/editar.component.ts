5f48b7855620eb114ac3be50dba580660ff6fbcc
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-26 16:24:57 -0300

    Criando GetById e o Edit do Funcionario

--- FrontEndAngular/src/app/pages/editar/editar.component.ts
+++ FrontEndAngular/src/app/pages/editar/editar.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Funcionario } from 'src/app/models/Funcionarios';
+import { FuncionarioService } from 'src/app/services/funcionario.service';
 
 @Component({
-  selector: 'app-editar',
-  templateUrl: './editar.component.html',
-  styleUrls: ['./editar.component.css']
+    selector: 'app-editar',
+    templateUrl: './editar.component.html',
+    styleUrls: ['./editar.component.css']
 })
-export class EditarComponent {
+export class EditarComponent implements OnInit {
 
+    btnAcao = "Editar";
+    btnTitulo = "Editar Funcionário!";
+    // Exclamacao por que sempre vai ser recebido
+    funcionario!: Funcionario;
+
+    constructor(private funcionarioService: FuncionarioService, private router: Router, private route: ActivatedRoute) {
+
+    }
+
+    ngOnInit(): void {
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+        console.log(id)
+
+        this.funcionarioService.GetFuncionario(id).subscribe((data) => {
+            console.log(data)
+            this.funcionario = data.dados;
+        });
+    }
+
+    editFuncionario(funcionario: Funcionario) {
+
+        this.funcionarioService.EditFuncionario(funcionario).subscribe(data => {
+            this.router.navigate(['/']);
+        });
+    }
 }
