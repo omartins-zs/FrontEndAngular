66f497563fafefdfb04013182f67d10988fbe2c4
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-27 14:37:42 -0300

    Ajuste no Endpoint de InativaFuncionario

--- FrontEndAngular/src/app/services/funcionario.service.ts
+++ FrontEndAngular/src/app/services/funcionario.service.ts
@@ -36,6 +36,11 @@ export class FuncionarioService {
     }
 
     // Endpoint PUT - InativaFuncionario
+    /**
+     * Realiza uma requisição PUT para inativar um funcionário.
+     * @param id O identificador do funcionário a ser inativado.
+     * @returns Um Observable que emite uma Response contendo um array de objetos do tipo Funcionario[].
+     */
     InativaFuncionario(id: number): Observable<Response<Funcionario[]>> {
         return this.http.put<Response<Funcionario[]>>(`${this.apiUrl}/inativaFuncionario?id=${id}`, id);
     }
