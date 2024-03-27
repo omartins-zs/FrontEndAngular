8c41903fc21ebad7c68bbe1d3b52b400cf4e25b6
omartins-zs <gabrielmatheus209@gmail.com>
2024-03-27 11:53:20 -0300

    Add Logica Get Funcionario e Id para os Detalhes

--- FrontEndAngular/src/app/pages/detalhes/detalhes.component.ts
+++ FrontEndAngular/src/app/pages/detalhes/detalhes.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Funcionario } from 'src/app/models/Funcionarios';
+import { FuncionarioService } from 'src/app/services/funcionario.service';
 
 @Component({
-  selector: 'app-detalhes',
-  templateUrl: './detalhes.component.html',
-  styleUrls: ['./detalhes.component.css']
+    selector: 'app-detalhes',
+    templateUrl: './detalhes.component.html',
+    styleUrls: ['./detalhes.component.css']
 })
-export class DetalhesComponent {
+export class DetalhesComponent implements OnInit {
+
+    funcionario?: Funcionario;
+
+    constructor(private funcionarioService: FuncionarioService, private route: ActivatedRoute, private router: Router) { }
+
+    ngOnInit(): void {
+
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+
+        this.funcionarioService.GetFuncionario(id).subscribe((data) => {
+            console.log(data)
+            this.funcionario = data.dados;
+        });
+
+        throw new Error('Method not implemented.');
+    }
+
+
 
 }
