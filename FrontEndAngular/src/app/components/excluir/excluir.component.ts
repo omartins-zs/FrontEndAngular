fdf2885e89dba5a82a4bfc628f27c30262b0ca57
omartins-zs <gabrielmatheus209@gmail.com>
2024-04-01 10:53:27 -0300

    Create o Construct,Import e Dependencias do Delete

--- FrontEndAngular/src/app/components/excluir/excluir.component.ts
+++ FrontEndAngular/src/app/components/excluir/excluir.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { FuncionarioService } from 'src/app/services/funcionario.service';
 
 @Component({
-  selector: 'app-excluir',
-  templateUrl: './excluir.component.html',
-  styleUrls: ['./excluir.component.css']
+    selector: 'app-excluir',
+    templateUrl: './excluir.component.html',
+    styleUrls: ['./excluir.component.css']
 })
-export class ExcluirComponent {
+export class ExcluirComponent implements OnInit {
+
+    constructor(
+        private FuncionarioService: FuncionarioService,
+        private router: Router,
+        @Inject(MAT_DIALOG_DATA) public data: any) {
+
+    }
+
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
 
 }
